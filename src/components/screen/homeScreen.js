ee2dcb2d4012090bcdd884e166b0ea72f3dd4b84
MepuDuaH <colephelps73@gmail.com>
2019-10-10 13:16:58 +0300

    apk

--- src/components/screen/homeScreen.js
+++ src/components/screen/homeScreen.js
@@ -5,21 +5,8 @@ import {fetchData} from '../../store/actions';
 import {Header, ImageList} from '../uikit';
 
 
-// const url = 'https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0';
-
 class HomeScreen extends Component {
-  // state = {
-  //   data: []
-  // }
-  // componentDidMount = async() => {
-  //   try {
-  //     const res = await fetch(url);
-  //     const data = await res.json();
-  //     this.setState({data});
-  //   } catch (error) {
-  //     throw error
-  //   }
-  // } 
+
 
   onFetchData = (newPhotos) => {
     this.props.fetchData(newPhotos);
@@ -27,7 +14,6 @@ class HomeScreen extends Component {
 
 
   render() {
-    console.log(this.props)
     const {navigation, data} = this.props;
     return (
       <ScrollView>
